02013bf50c176a1eec502cbdf4413f71ed7d0215
clumerze <clumerze@outlook.com>
2020-06-27 08:01:27 +0300

    implemented BTreeIndex

--- src/main/java/com/gridnine/testing/service/FlightFilter.java
+++ src/main/java/com/gridnine/testing/service/FlightFilter.java
@@ -10,12 +10,8 @@ import java.util.function.Predicate;
 
 public interface FlightFilter {
     List<Flight> byCondition(Predicate<? super Flight> condition);
-
     <P> List<Flight> byProperty(Function<? super Flight, P> property, Predicate<? super P> condition);
-
     <P> List<Flight> byCollectionProperty(Function<? super Flight, Collection<P>> property, Predicate<? super P> condition);
-
     List<Flight> earlyFlight(LocalDateTime time);
-
     List<Flight> lateFlight(LocalDateTime time);
 }
