a0ed1fed361df94ad93cd7ea07251dc1c5f8a2d7
clumerze <clumerze@outlook.com>
2020-06-29 04:01:36 +0300

    view

--- src/main/java/com/gridnine/testing/service/ComparableIndex.java
+++ src/main/java/com/gridnine/testing/service/ComparableIndex.java
@@ -2,7 +2,7 @@ package com.gridnine.testing.service;
 
 import java.util.List;
 
-public interface ComparableIndex<P extends Comparable<? extends P>, T> {
+public interface ComparableIndex<P extends Comparable<P>, T> {
     List<T> more(P key);
 
     List<T> less(P key);
